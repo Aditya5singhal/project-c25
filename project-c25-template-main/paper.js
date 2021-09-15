db8aeb3f4c5c257f8d9bb465b5611c6ad539ddcb
Aditya5singhal <77671226+Aditya5singhal@users.noreply.github.com>
2021-09-15 10:22:55 +0530

    Add files via upload

---
+++ project-c25-template-main/paper.js
@@ -0,0 +1,34 @@
+class paper 
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		
+		
+		this.image=loadImage("paper.png")
+		function keyPressed(){
+    if (keyCode===UP_ARROW){
+  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:145})
+
+  Matter.Body.applyForce()
+    }
+        }
+	}
+	display()
+	{
+
+var options={
+    isStatic:false,
+    restitution:0.3,
+    friction:0,
+density:1.2,
+
+}
+
+
+}
+
+
+
+}
